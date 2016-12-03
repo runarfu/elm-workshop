47c3af8ebee5d8801fb0d66127a7d56bb6c6bd57
ruf <ruf@knowit.no>
2016-12-03 13:40:35 +0100

    Exercises in 02

--- src/02/Main.elm
+++ src/02/Main.elm
@@ -3,6 +3,17 @@ module Main exposing (..)
 import Html exposing (..)
 
 
+-- TODO 2.0 Introduce a type to replace the type variable `a`.
+--
+-- TODO 2.1 Add a button to the view.
+--
+-- TODO 2.2 Make Elm send a message to `update` (of the type you introduced in 2.0)
+--          when the button is pressed.
+--
+-- TODO 2.3 Make your `update`-function do something with the model when the
+--          button has been pressed. For instance, append some text to the model.
+
+
 main : Program Never String a
 main =
     Html.beginnerProgram
@@ -14,7 +25,10 @@ main =
 
 view : String -> Html a
 view model =
-    h1 [] [ text model ]
+    div []
+        [ h1 [] [ text "Exercise 2" ]
+        , p [] [ text model ]
+        ]
 
 
 update : a -> String -> String
