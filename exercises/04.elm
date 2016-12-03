3af66ff49fc7440cde9d92570e4ad37aac0d573d
ruf <ruf@knowit.no>
2016-12-03 16:45:25 +0100

    Exercise 4

---
+++ exercises/04.elm
@@ -0,0 +1,48 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (..)
+
+
+-- TODO 4.0 Update the `userInput`-field in the model when text is entered in
+--          the textarea.
+--          Hint: Only 2 lines need to be edited, one in the view-function and
+--                another in the update-function.
+
+
+main : Program Never Model Msg
+main =
+    beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
+
+
+initialModel : Model
+initialModel =
+    { userInput = "" }
+
+
+type alias Model =
+    { userInput : String }
+
+
+type Msg
+    = TextEntered String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        TextEntered string ->
+            model
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "Exercise 4" ]
+        , textarea [] []
+        , p [] [ text (toString model) ]
+        ]
