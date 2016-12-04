ef7b8bb05158f52699e0b5e5ff17340474ac1e96
ruf <ruf@knowit.no>
2016-12-04 12:35:47 +0100

    Simplified ex01

--- exercises/01.elm
+++ exercises/01.elm
@@ -1,6 +1,6 @@
 module Main exposing (..)
 
-import Html exposing (beginnerProgram, h1, text)
+import Html exposing (..)
 
 
 -- TODO 1.0 Build this app with `elm make --warn Main.elm` or run it with `elm reactor`.
@@ -11,15 +11,7 @@ import Html exposing (beginnerProgram, h1, text)
 --          and show you the result. Any changes to `Main.elm` can be recompiled
 --          simply by refreshing the page in the browser.
 --
--- TODO 1.2 Two of the top-level values in this file (view and update) lack
---          type annotations. `elm make --warn` will tell you this.
---          Fix this by adding type annotations.
---          Hint: If you struggle with finding out which types you should use,
---                try using a type which is clearly *wrong*. Then Elm will give
---                give you an error message containing which types were *inferred*.
---                In many cases these will be the correct types.
---
--- TODO 1.3 Display the model value `1337` somehow.
+-- TODO 1.2 Display the model value `1337` somehow.
 
 
 main : Program Never Int a
@@ -31,9 +23,11 @@ main =
         }
 
 
-view _ =
+view : Int -> Html a
+view model =
     h1 [] [ text "Exercise 1" ]
 
 
+update : a -> Int -> Int
 update _ m =
     m
