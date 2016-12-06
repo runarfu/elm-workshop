c6d8461253ad39adbf5e59f30e4f4ae1abc4ef4d
ruf <ruf@knowit.no>
2016-12-06 19:56:02 +0100

    Missing import

--- exercises/04.elm
+++ exercises/04.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
 -- TODO 4.0 Update the `userInput`-field in the model when text is entered in
