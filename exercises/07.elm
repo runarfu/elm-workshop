8160a223c8de5798c7cc4282e494b4da0232ac67
ruf <ruf@knowit.no>
2016-12-07 15:27:53 +0100

    New exercise

--- exercises/07.elm
+++ exercises/07.elm
@@ -2,11 +2,11 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+import String
 
 
--- TODO 6.0 Displaying two boolean values is boring. Instead, style the display
---          of `wordsToWrite` so that it's easy to see how far in the `wordsToWrite`
---          you've come.
+-- TODO 7.0
 
 
 main : Program Never Model Msg
@@ -46,9 +46,35 @@ view : Model -> Html Msg
 view model =
     div []
         [ h1 [] [ text "Exercise 6" ]
-        , p [] [ text model.wordsToWrite ]
+        , viewWordsToWriteWithHighlighting model
         , textarea [ onInput TextEntered ] []
         , p [] [ text ("Input is prefix of wordsToWrite: " ++ (toString (String.startsWith model.userInput model.wordsToWrite))) ]
         , p [] [ text ("Input equals wordsToWrite: " ++ (toString (model.userInput == model.wordsToWrite))) ]
         , p [] [ text (toString model) ]
         ]
+
+
+viewWordsToWriteWithHighlighting : Model -> Html Msg
+viewWordsToWriteWithHighlighting model =
+    let
+        splitOnPrefix s1 s2 =
+            if String.startsWith s1 s2 then
+                Just ( s1, String.dropLeft (String.length s1) s2 )
+            else
+                Nothing
+
+        correctStyle =
+            style [ ( "color", "green" ), ( "background-color", "gray" ) ]
+
+        wrongStyle =
+            style [ ( "color", "red" ), ( "background-color", "gray" ) ]
+    in
+        case splitOnPrefix model.userInput model.wordsToWrite of
+            Just ( firstPart, rest ) ->
+                div []
+                    [ span [ correctStyle ] [ text firstPart ]
+                    , span [] [ text rest ]
+                    ]
+
+            Nothing ->
+                div [ wrongStyle ] [ text model.wordsToWrite ]
